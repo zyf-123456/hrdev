3bdbc3615d933c63630dba79764c6cefca688f24
zyf-123456 <1718346296@qq.com>
2020-01-16 17:23:32 +0800

    字符串不对

--- hrdev/src/main/java/com/example/hrdev/dao/TestMapper.java
+++ hrdev/src/main/java/com/example/hrdev/dao/TestMapper.java
@@ -5,6 +5,5 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface TestMapper {
-
     TestVo test();
 }
--- hrdev/src/main/java/com/example/hrdev/entity/TestVo.java
+++ hrdev/src/main/java/com/example/hrdev/entity/TestVo.java
@@ -1,24 +1,18 @@
 package com.example.hrdev.entity;
 
 public class TestVo {
-
     private String id;
-
     private String name;
-
     public String getId() {
         return id;
     }
-
     public void setId(String id) {
         this.id = id;
     }
-
     public String getName() {
         return name;
     }
-
-    public void setName(String name) {
+    public void setName(String names) {
         this.name = name;
     }
 }
